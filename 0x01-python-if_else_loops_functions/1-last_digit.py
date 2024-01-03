47315c0107828c405d22338846dfd315b05abf3b
FadilAnas <anasfadili65@gmail.com>
2024-01-03 11:54:22 -0800

    louby

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# this nex part it's used to know if the
+if number < 0:
+    number2 = number * -1
+else:
+    number2 = number
+modulo = number2 % 10
+# modulo is a mathimatical opertator to have the rest of the division
+if modulo > 5:
+    print("Last digit of", number, "is", modulo, "and is greater than 5")
+elif modulo == 0:
+    print("Last digit of", number, "is", modulo, "and is 0")
+else:
+    print("Last digit of", number, "is", modulo, "and is less than 6 and not 0")
+
