8236190fb3e05609e7339856cc76429abde3713b
FadilAnas <anasfadili65@gmail.com>
2024-01-03 12:30:56 -0800

    louby

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,12 +7,13 @@ if number < 0:
 else:
     number2 = number
 modulo = number2 % 10
+if number < 0:
+    modulo = modulo * -1
 # modulo is a mathimatical opertator to have the rest of the division
 if modulo > 5:
     print("Last digit of", number, "is", modulo, "and is greater than 5")
 elif modulo == 0:
     print("Last digit of", number, "is", modulo, "and is 0")
 elif modulo < 6 and modulo != 0:
-    modulo = modulo * -1
     print("Last digit of", number, "is", modulo, end=" ")
     print("and is less than 6 and not 0")
