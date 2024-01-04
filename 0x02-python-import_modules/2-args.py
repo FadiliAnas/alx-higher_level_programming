e38e3b6800e64a9fcaf976ba8b9837d374736b57
FadilAnas <anasfadili65@gmail.com>
2024-01-04 12:57:25 -0800

    louby

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     i = 0
-    if (len(sys.argv)-1) != 0:
+    if len(sys.argv)-1 > 1:
         print("{} arguments:".format(len(sys.argv)-1))
         while i + 1 < len(sys.argv):
             print("{}: {}".format(i+1, sys.argv[i + 1]))
@@ -13,3 +13,4 @@ if __name__ == "__main__":
         print("{}: {}".format(i+1, sys.argv[i + 1]))
     else:
         print("{} arguments.".format(len(sys.argv)-1))
+        
