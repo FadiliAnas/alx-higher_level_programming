c6ba75919b4af3035305e361bfdc61967c9002df
FadilAnas <anasfadili65@gmail.com>
2024-01-06 14:59:49 -0800

    louby

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # Loop through each row in the matrix
     i = 0
+    if matrix == [[]]:
+        print()
     while i < len(matrix):
-        # Initialize j for the inner loop
         j = 0
         while j < len(matrix[i]):
-            # Print the element at the current position
-            print("{:d}".format(matrix[i][j]), end=" ")
+            # Print the element at the current position followed by a space
+            
+            if j == len(matrix) - 1:
+                print("{:d}".format(matrix[i][j]))
+            else:
+                print("{:d}".format(matrix[i][j]), end=" ")
             j += 1
-        print("$", end=" ")
-        print()  # Move to the next line after printing each row
         i += 1
