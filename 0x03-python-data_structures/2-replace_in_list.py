a85fd02045485b0a8559cb7988d52cf303525a92
FadilAnas <anasfadili65@gmail.com>
2024-01-06 10:12:34 -0800

    louby

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return None
+    elif idx > lenght(my_list[:]) - 1:
+        return None
+    else:
+        new_my_list = my_list.copy()
+        new_my_list[idx] = element
+        return new_my_list
