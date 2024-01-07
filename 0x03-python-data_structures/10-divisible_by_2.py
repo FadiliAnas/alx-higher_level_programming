b0079667f2b3305adafc6f0ac3dc83fa0ad3f59f
FadilAnas <anasfadili65@gmail.com>
2024-01-07 11:06:23 -0800

    louby

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    i = 0
+    x = []
+    while i < len(my_list):
+        if my_list[i] % 2 == 0:
+            x.append(True)
+        elif my_list[i] % 2 != 0:
+            x.append(False)
+        i += 1
+    return x
