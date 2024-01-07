4006d864d34ff691cd789ada5c28534c574bdd17
FadilAnas <anasfadili65@gmail.com>
2024-01-07 10:21:25 -0800

    louby

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     max_int = 0
     i = 0
     if len(my_list) == 0:
-        return None
+        max_int = None
     while i < len(my_list):
 
         if max_int < my_list[i]:
