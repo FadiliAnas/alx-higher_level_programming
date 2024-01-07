6a2a3125f073c68f76e27a11edf14a82648a4289
FadilAnas <anasfadili65@gmail.com>
2024-01-07 09:39:18 -0800

    louby

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
     if length == 0:
-        first = None
-
+        sentence[0] = None
+    first = sentence[0]
     return (length, first)
