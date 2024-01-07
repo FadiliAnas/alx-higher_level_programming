5af16c016c80c8b0c0cc4849377921ae3fabb80b
FadilAnas <anasfadili65@gmail.com>
2024-01-07 11:36:03 -0800

    louby

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    new_list = my_list.copy()
+    if idx < len(my_list) or len(my_list) == 0:
+        return my_list
+    else:
+        del (new_list[idx])
+    return new_list
