d2004ef3250515022221a9081de618f2a5da3691
root <root@DESKTOP-3AONHLE.localdomain>
2024-12-23 20:25:32 +0100

    pushing the ALX tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Define a class named Square"""
+
+    def __init__(self, size):
+        """Constructor:
+
+        Args:
+            size: length of a side of the square
+        """
+        self.__size = size
